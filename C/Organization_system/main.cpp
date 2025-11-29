a2899c2291699009ff81cb02fc9808c50738427f
AnnnnU <a1251116144@qq.com>
2025-11-29 23:35:56 +0800

    2025_11_29_2

--- C/Organization_system/main.cpp
+++ C/Organization_system/main.cpp
@@ -16,10 +16,12 @@ class Contactperson
     {
         return address;
     }
+
     string get_name() const
     {
         return name;
     }
+
     string get_phonenumber(int index) const
     {
         if (index >= 0 && index < phonenumber.size()){
@@ -39,6 +41,7 @@ class Contactperson
     {
         name=nameget,address=addressget,phonenumber.push_back(phonenumberget);
     }
+
     void add_phonenumber(string new_phonenumber)
     {
         phonenumber.push_back(new_phonenumber);
@@ -70,7 +73,7 @@ void findContactPerson()
     cin >> user_choice;
 
     if (user_choice == 1) {
-        cout << "请输入联系人姓名：\n";
+        cout << "请输入魔女姓名：\n";
         string name;
         cin >> name;
 
@@ -89,7 +92,7 @@ void findContactPerson()
     }
     else if (user_choice == 2) {
         string phonenumber_demo;
-        cout << "请输入联系人电话号码：\n";
+        cout << "请输入魔女电话号码：\n";
         cin >> phonenumber_demo;
         
     }
@@ -100,6 +103,21 @@ void modifyContactPerson()
 {
 
 }
+
+void addContactPerson(){
+    string name,address,phonenumber;
+    cout << "请输入魔女姓名：" << endl;
+    cin >> name;
+    cout << "请输入魔女地址：" << endl;
+    cin >> address;
+    cout << "请输入魔女电话号码：" << endl;
+    cin >> phonenumber;
+    Contactperson contact_temp;
+    contact_temp.init_contact(name,address,phonenumber);
+    contactpersonlist.push_back(contact_temp);
+    cout << "魔女添加成功！" << endl;
+}
+
 void clearAllContactPerson()
 {
 
@@ -107,16 +125,116 @@ void clearAllContactPerson()
 
 void showmenu()
 {
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << "**********************" << endl;
+    cout << "*******魔女图鉴********" << endl;
+    cout << "*****1、添加魔女*******" << endl;
+    cout << "*****2、显示魔女*******" << endl;
+    cout << "*****3、删除魔女*******" << endl;
+    cout << "*****4、查找魔女*******" << endl;
+    cout << "*****5、修改魔女*******" << endl;
+    cout << "*****6、清空魔女*******" << endl;
+    cout << "****0、退出魔女图鉴*****" << endl;
+    cout << "***********************" << endl;
+}
 
-    cout << "**********************" << endl;
-    cout << "*****1、添加联系人*****" << endl;
-    cout << "*****2、显示联系人*****" << endl;
-    cout << "*****3、删除联系人*****" << endl;
-    cout << "*****4、查找联系人*****" << endl;
-    cout << "*****5、修改联系人*****" << endl;
-    cout << "*****6、清空联系人*****" << endl;
-    cout << "*****0、退出通讯录*****" << endl;
-    cout << "**********************" << endl;
+void deleteContactPerson()
+{
+    cout << "输入想要删除的魔女序号:\n";
+
+    // 列出所有魔女（带序号）
+    for (size_t contactIdx = 0; contactIdx < contactpersonlist.size(); ++contactIdx)
+    {
+        const auto& cp = contactpersonlist[contactIdx];
+        cout << contactIdx << ". " << cp.get_name() << '\n';
+    }
+
+    // 用户输入序号
+    size_t userChoice= -1;
+    //默认无效
+    cin >> userChoice;
+
+    if (userChoice >= contactpersonlist.size() )
+    {
+        cout << "无效的魔女序号！\n";
+        return;
+    }
+
+    // 删除所选魔女
+    contactpersonlist.erase(contactpersonlist.begin() + userChoice);
+    cout << "魔女已删除。\n";
+}
+
+void showContactPerson()
+{
+    cout << "输入想要查询的魔女序号:\n";
+
+    // 列出所有魔女（带序号）
+    for (size_t contactIdx = 0; contactIdx < contactpersonlist.size(); ++contactIdx)
+    {
+        const auto& cp = contactpersonlist[contactIdx];
+        cout << contactIdx << ". " << cp.get_name() << '\n';
+    }
+
+    // 用户输入序号
+    size_t userChoice= -1;
+    //默认无效
+    cin >> userChoice;
+
+    if (userChoice >= contactpersonlist.size() && userChoice < 0)
+    {
+        cout << "无效的魔女序号！\n";
+        return;
+    }
+
+    // 输出所选魔女详情
+    const auto& cp = contactpersonlist[userChoice];
+    cout << "用户名字：" << cp.get_name() << '\n'
+         << "用户地址：" << cp.get_address() << '\n'
+         << "用户电话：";
+    for (size_t i = 0; i < cp.get_phonenumber_amount(); ++i)
+    {
+        cout << cp.get_phonenumber(i);
+        if (i + 1 < cp.get_phonenumber_amount()) cout << " | ";
+    }
+    cout << '\n';
+}
+
+ void contactpersoninit(){
+    // 13 位魔法少女初始化
+Contactperson ema;      ema.init_contact("樱羽艾玛",    "三木谷奈奈",      "13800138000");
+Contactperson hiro;     hiro.init_contact("二阶堂希罗",  "東雲はる",        "13900139000");
+Contactperson coco;     coco.init_contact("泽渡可可",    "比良坂芽衣",      "13700137000");
+Contactperson shirley;  shirley.init_contact("橘雪莉",   "柊优花",          "13600136000");
+Contactperson hanna;    hanna.init_contact("远野汉娜",   "石崎纱彩",        "13500135000");
+Contactperson ann;      ann.init_contact("夏目安安",     "葵梓",            "13400134000");
+Contactperson noah;     noah.init_contact("城崎诺亚",   "井口裕香",        "13300133000");
+Contactperson reira;    reira.init_contact("莲见蕾雅",  "小清水亚美",      "13200132000");
+Contactperson miria;    miria.init_contact("佐伯米莉亚","高森奈津美",      "13100131000");
+Contactperson nayuka;   nayuka.init_contact("黑部奈叶香","大熊和奏",       "13000130000");
+Contactperson mag;      mag.init_contact("宝生玛格",    "树冬华",          "12900129000");
+Contactperson arisa;    arisa.init_contact("紫藤亚里沙","石井未纱",        "12800128000");
+Contactperson meruru;   meruru.init_contact("冰上梅露露","山下七海",        "12700127000");
+
+contactpersonlist.emplace_back(ema);
+contactpersonlist.emplace_back(hiro);
+contactpersonlist.emplace_back(coco);
+contactpersonlist.emplace_back(shirley);
+contactpersonlist.emplace_back(hanna);
+contactpersonlist.emplace_back(ann);
+contactpersonlist.emplace_back(noah);
+contactpersonlist.emplace_back(reira);
+contactpersonlist.emplace_back(miria);
+contactpersonlist.emplace_back(nayuka);
+contactpersonlist.emplace_back(mag);
+contactpersonlist.emplace_back(arisa);
+contactpersonlist.emplace_back(meruru);
+
 }
 
 int main()
@@ -124,9 +242,7 @@ int main()
     SetConsoleOutputCP(CP_UTF8);   // 输出 UTF-8
     SetConsoleCP(CP_UTF8); 
     int select = 0;
-    Contactperson li;
-    li.init_contact("李四","北京市海淀区","13800138000");
-    contactpersonlist.push_back(li);
+    contactpersoninit();
     while(true){
     showmenu();
     cin >> select;
@@ -134,22 +250,22 @@ int main()
     cout << "欢迎下次再来。" << endl;
     return 0;
     }
-    else if (select == 1) {                     // 1. 添加联系人
-    //addContactPerson();
+    else if (select == 1) {                     // 1. 添加魔女
+    addContactPerson();
     }
-    else if (select == 2) {                     // 2. 显示联系人
-    //showContactPerson();
+    else if (select == 2) {                     // 2. 显示魔女
+    showContactPerson();
     }
-    else if (select == 3) {                     // 3. 删除联系人
-    //deleteContactPerson();
+    else if (select == 3) {                     // 3. 删除魔女
+    deleteContactPerson();
     }
-    else if (select == 4) {                     // 4. 查找联系人
+    else if (select == 4) {                     // 4. 查找魔女
     findContactPerson();
     }
-    else if (select == 5) {                     // 5. 修改联系人
+    else if (select == 5) {                     // 5. 修改魔女
     modifyContactPerson();
     }
-    else if (select == 6) {                     // 6. 清空联系人
+    else if (select == 6) {                     // 6. 清空魔女
     clearAllContactPerson();
     }
     }
